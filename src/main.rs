2b9598dc2179ef388f522bfb69cca1b04b9ea525
Arturo Gonzalez <201297720+GeluMori@users.noreply.github.com>
2026-03-02 23:05:48 -0600

    able to get HOME dir and able to make file .taskvault in home dir

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::path::PathBuf;
+
+fn main() -> std::io::Result<()> {
+    let key = "HOME";
+    match env::var(key) {
+        Ok(val) => {
+            let home = PathBuf::from(val);
+            let taskvault_dir = home.join(".taskvault");
+            println!("{}", taskvault_dir.display());
+            std::fs::create_dir_all(&taskvault_dir)?;
+        }
+        Err(e) => println!("couldn't interpret {key}: {e}", key=key, e=e),
+    }
+    Ok(())
 }
